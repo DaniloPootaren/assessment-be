f2edc3463560be3ee7c29693fb1d85ea8c37403d
Jean Danilo Maxwell Patrick Pootaren <danilo.pootaren@gmail.com>
2020-08-03 20:32:23 +0400

    initial commits

---
+++ assessment-be/src/main/java/com/danilopootaren/assessment/AssessmentBeApplication.java
@@ -0,0 +1,13 @@
+package com.danilopootaren.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentBeApplication.class, args);
+	}
+
+}
---
+++ assessment-be/src/test/java/com/danilopootaren/assessment/AssessmentBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilopootaren.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
